7fb22df04d08f3ffa7e3c765e1203c2de67efa9b
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-06-06 23:24:17 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'Caleb', // Your name
   ownerNumber: '2348104040841', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.2",
 };

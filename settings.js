d169bc428295ced95c25b3e06a79cc651fdbddb6
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-06-06 21:35:14 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'K4l3b Bot',
+  author: 'caleb‎',
+  botName: "K4l3b Bot",
+  botOwner: 'Caleb', // Your name
+  ownerNumber: '2348104040841', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.2",
+};
+
+module.exports = settings;

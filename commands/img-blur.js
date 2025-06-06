912f8e8c1f8890ad5157c69f1cd5a501f1dc090b
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-06-06 22:32:08 +0100

    img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363402198872825@newsletter',
+                    newsletterName: 'K4l3bBot MD',
                     serverMessageId: -1
                 }
             }

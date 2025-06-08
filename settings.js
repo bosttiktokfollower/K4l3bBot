53ac769677b3c443c1ef445704455f5834693739
bosttiktokfollower <camxyblink@gmail.com>
2025-06-08 13:52:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'K4l3b Bot',
-  author: 'caleb‎',
+  author: 'Kleb‎',
   botName: "K4l3b Bot",
-  botOwner: 'Caleb', // Your name
+  botOwner: 'Kleb', // Your name
   ownerNumber: '2348104040841', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
